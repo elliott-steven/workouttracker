4ac63a98c0ce34a2d156992a080b5aac08ecd3b1
Steven Elliott <steven.elliott@live.com>
2021-04-21 12:01:39 -0400

    updated package.json and added starter files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const apiRoute = require("./routes/apiRoute.js");
+const htmlRoute = require("./routes/htmlRoute.js")
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+app.use(htmlRoute);
+app.use(apiRoute);
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
