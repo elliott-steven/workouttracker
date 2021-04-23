df6efeb7fd3b8d5753588ecdb5186351b55b16a9
Steven Elliott <steven.elliott@live.com>
2021-04-22 21:38:54 -0400

    creating model for workout.js

---
+++ models/workout.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter your exercise type"
+            },
+
+            duration: {
+                type: Number,
+                required: "Enter your exercise duration in minutes"
+            },
+
+            distance: {
+                type: Number
+            },
+
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter your exercise name"
+            },
+
+            reps: {
+                type: Number
+            },
+
+            sets: {
+                type: Number
+            },
+
+            weight: {
+                type: Number
+            }
+        }
+    ]
+});
+
+workoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+module.exports = Workout;
