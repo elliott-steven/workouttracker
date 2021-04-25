256d8a101327e21883af7346cf97b925e344336d
Steven Elliott <steven.elliott@live.com>
2021-04-24 20:19:45 -0400

    finished routes

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -4,6 +4,7 @@ const Workout = require("../models/workout");
 router.post("/api/workouts", ({body},res) => {
 
     Workout.create(body)
+    
     .then(workoutDB => {
         res.json(workoutDB);
     })
@@ -17,6 +18,7 @@ router.put("/api/workouts/:id", ({body,params},res) => {
     console.log(body)
 
     Workout.findByIdAndUpdate(params.id, {$push: {exercises: body}}, {new: true})
+
     .then(workoutDB => {
         res.json(workoutDB);
     })
@@ -25,3 +27,47 @@ router.put("/api/workouts/:id", ({body,params},res) => {
         res.status(400).json(err);
     });
 });
+
+router.get("/api/workouts", (req, res) => {
+
+    Workout.find({})
+        .then(workoutDB => {
+            res.json(workoutDB);
+        })
+
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+
+    Workout.find({})
+        
+        .limit(10)
+
+        .then(workoutDB => {
+            res.json(workoutDB);
+        })
+
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+
+    Workout.findByIdAndRemove(body.id)
+
+        .then(() => {
+            res.json(true);
+        })
+
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+
+
+module.exports = router;
